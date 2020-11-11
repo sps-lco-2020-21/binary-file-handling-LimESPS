4a875ffa0f00bb358e3b7d64beeadcf65ec559f2
LimESPS <72739674+LimESPS@users.noreply.github.com>
2020-11-11 09:58:50 +0000

    BitMapFileCode -Ethan

---
+++ .github/BitMapFiles/BitMapFiles.App/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace BitMapFiles.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte[] logo = File.ReadAllBytes("sps.bmp");
+            Console.WriteLine($"Is file a BM file?: {BM(logo)}");
+            Console.ReadKey();
+        }
+        static bool BM(byte[] logo)
+        {
+            byte first = logo[0];
+            byte second = logo[1];
+            char b = 'B';
+            char m = 'M';
+            int res1 = first ^ Convert.ToByte(b);
+            int res2 = second ^ (byte)m;
+            if (res1 == 0 && res2 == 0)
+                return true;
+            else
+                return false;
+        }
+    }
+}
